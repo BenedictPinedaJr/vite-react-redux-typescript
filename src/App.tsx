fbcec89c9be4183730434b5a55872be5d71fb773
BenedictPinedaJr <benedictpinedajr@gmail.com>
2022-10-26 15:34:19 +0800

    Created the side navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
-import { BottomNavbar, Button } from "./components/common";
+import { BottomNavbar, Button, SideNavbar } from "./components/common";
 import { changeTheme } from "./redux/features/app-theme";
-
 import { useAppDispatch, useAppSelector } from "./redux/hooks";
-
 import { BsMusicNote, BsMusicNoteList } from "react-icons/bs";
 import { MdDarkMode, MdLightMode } from "react-icons/md";
 import { Outlet } from "react-router-dom";
 import MusicPlayer from "./components/features/music-player";
 import classNames from "classnames";
+import logo from "./assets/react.svg";
 
 const App = () => {
   const isDarkMode = useAppSelector((state) => state.appTheme.isDarkMode);
@@ -19,31 +18,38 @@ const App = () => {
   };
 
   return (
-    <div
-      className={classNames(
-        "flex h-screen flex-col",
-        isDarkMode
-          ? "bg-gradient-to-r from-indigo-900 to-indigo-600"
-          : "bg-gradient-to-r from-sky-500 to-indigo-500"
-      )}
-    >
-      <Button onClick={changeAppTheme} asIconButton className="w-12 mx-auto">
-        {isDarkMode ? <MdLightMode size={32} /> : <MdDarkMode size={32} />}
-      </Button>
-      <Outlet />
-      {activeSong?.title && (
-        <MusicPlayer />
-      )}
-      <BottomNavbar
-        iconLinks={[
-          { icon: <BsMusicNote size={32} />, to: "/", label: "Music" },
-          {
-            icon: <BsMusicNoteList size={32} />,
-            to: "discover",
-            label: "Discover",
-          },
+    <div className="relative flex">
+      <SideNavbar
+        links={[
+          { to: "discover", label: "Discover" },
+          { to: "/", label: "Music" },
         ]}
+        logo={logo}
       />
+      <div
+        className={classNames(
+          "flex h-screen w-screen flex-col",
+          isDarkMode
+            ? "bg-gradient-to-r from-indigo-900 to-indigo-600"
+            : "bg-gradient-to-r from-sky-500 to-indigo-500"
+        )}
+      >
+        <Button onClick={changeAppTheme} asIconButton className="mx-auto w-12">
+          {isDarkMode ? <MdLightMode size={32} /> : <MdDarkMode size={32} />}
+        </Button>
+        <Outlet />
+        {activeSong?.title && <MusicPlayer />}
+        <BottomNavbar
+          iconLinks={[
+            { icon: <BsMusicNote size={32} />, to: "/", label: "Music" },
+            {
+              icon: <BsMusicNoteList size={32} />,
+              to: "discover",
+              label: "Discover",
+            },
+          ]}
+        />
+      </div>
     </div>
   );
 };
--- src/components/common/side-navbar/side-navbar.tsx
+++ src/components/common/side-navbar/side-navbar.tsx
@@ -2,17 +2,41 @@ import classNames from "classnames";
 import { NavLink } from "react-router-dom";
 import { v4 as uuidv4 } from "uuid";
 
+type Link = {
+  to: string;
+  label: string;
+};
+
 interface Props {
-
+  links: Link[];
+  dataTestId?: string;
+  logo?: string
 }
 
-const SideNavbar = ({}:Props) => {
-
+const SideNavbar = ({ links, dataTestId, logo }: Props) => {
   return (
-    <div>
-      
+    <div
+      className="hidden w-60 flex-col bg-[#191624] px-4 py-10 sm:flex"
+      data-testid={dataTestId}
+    >
+      <img src={logo} alt="Logo" className="w-full h-14 object-contain mb-4" />
+      {links.map((link) => (
+        <NavLink
+          key={uuidv4()}
+          to={link.to}
+          end={link.to === "/" ? true : false}
+          className={({ isActive }) =>
+            classNames(
+              "flex flex-col items-center rounded-lg p-2 text-white",
+              isActive ? "bg-slate-300 bg-opacity-50 font-bold" : undefined
+            )
+          }
+        >
+          {link.label}
+        </NavLink>
+      ))}
     </div>
-  )
-}
+  );
+};
 
 export default SideNavbar;
